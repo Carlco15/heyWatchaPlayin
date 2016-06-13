23e1af3ab7af3a165f7b4eef9dc108970347f084
Carl Onsgard <carlco15263@gmail.com>
2016-06-13 09:40:25 -0400

    Added bower components.

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,17 @@
 'use strict';
 
-angular.module('heyWatchaPlayinApp', ['heyWatchaPlayinApp.auth', 'heyWatchaPlayinApp.admin',
-    'heyWatchaPlayinApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io',
-    'ui.router', 'ui.bootstrap', 'validation.match'
+angular.module('heyWatchaPlayinApp', [
+               'heyWatchaPlayinApp.auth',
+               'heyWatchaPlayinApp.admin',
+               'heyWatchaPlayinApp.constants',
+               'ngCookies',
+               'ngResource',
+               'ngSanitize',
+               'btford.socket-io',
+               'ui.router',
+               'ui.bootstrap',
+               'validation.match',
+               'ngAnimate'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
